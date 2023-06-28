5ff843ed72453ca7665716486d1d7958897f09ff
Teddy Conyers Jr <tc3941@rit.edu>
2023-06-27 20:24:57 -0400

    Add changes as directed in the exercises

--- script.js
+++ script.js
@@ -5,5 +5,26 @@
 // 4. Color
 // Finally, make sure to add 4 methods (getMake, getModel, getYear, and getColor) that return the car's respective properties
 // (This is called a 'getter' in Object Oriented Programming)
+class Car {
+  constructor(make, model, year, color) {
+    this.make = make
+    this.model = model
+    this.year = year
+    this.color = color
+  }
+
+  get getMake() {
+    return this.make
+  }
+  get getModel() {
+    return this.model
+  }
+  get getYear() {
+    return this.year
+  }
+  get getColor() {
+    return this.color
+  }
+}
 
 // If you're stuck, take a look at the example class here for reference: https://www.geeksforgeeks.org/introduction-object-oriented-programming-javascript/#
